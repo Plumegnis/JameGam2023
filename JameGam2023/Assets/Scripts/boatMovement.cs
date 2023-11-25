1c0f3e66ad93ab69005ebac344be2d377bd9fa30
Plumegnis <osieckajulia@gmail.com>
2023-11-25 16:26:13 +0100

    stuff

---
+++ JameGam2023/Assets/Scripts/boatMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boatMovement : MonoBehaviour
+{
+    public GameObject lifeTalking;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeTalking.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            lifeTalking.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            lifeTalking.SetActive(true);
+        }
+    }
+}
