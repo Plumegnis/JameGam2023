1f71e32e6dca844896a72b4a14bc7d446a133198
Plumegnis <osieckajulia@gmail.com>
2023-11-26 19:37:12 +0100

    main menu play on spacebar

---
+++ JameGam2023/Assets/Scripts/fadeintutorial.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class fadeintutorial : MonoBehaviour
+{
+    public GameObject fadein;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fadein.activeInHierarchy)
+
+        {
+
+            if (Input.GetKeyDown(KeyCode.Space))
+
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        }
+    }
+
